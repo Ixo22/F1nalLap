29302a4b72966cee0607cdf3d2642fc5fc0bca8c
albeerto22 <120147657+Ixo22@users.noreply.github.com>
2025-04-07 16:13:40 +0200

    Cambios en scripts, .scss y .html

--- F1nalLap/src/app/app.routes.ts
+++ F1nalLap/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'temp-actual', loadComponent: () => import('./pages/temp-actual/temp-actual.component').then(m => m.TempActualComponent) }
 ];
  
 
---
+++ F1nalLap/src/app/pages/temp-actual/temp-actual.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-actual',
+  imports: [],
+  templateUrl: './temp-actual.component.html',
+  styleUrl: './temp-actual.component.scss'
+})
+export class TempActualComponent {
+
+}
