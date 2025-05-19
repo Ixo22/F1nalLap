d1b84e016f459a4f660287ab1aeb4619bb09cad8
albeerto22 <120147657+Ixo22@users.noreply.github.com>
2025-05-19 16:40:32 +0200

    Añadimos campos para la simulación y para mostrar las mejores estrategias calculadas

--- F1nalLap/src/app/pages/circuits/circuits.component.ts
+++ F1nalLap/src/app/pages/circuits/circuits.component.ts
@@ -40,6 +40,12 @@ export class CircuitsComponent implements OnInit {
   circuitControl = new FormControl('');
   selectedCircuit: any = null;
   bestStrategies: any[] = [];
+  bestStrategiesVisible = false;
+  simuladorActivo = false;
+  simuladorResultado: any = null;
+  simuladorStints: { compuesto: string, vueltas: number | null }[] = [];
+  simuladorParadas = 1;
+  compuestos: string[] = ['Blandos', 'Medios', 'Duros'];
 
   constructor(private http: HttpClient, @Inject(EstrategiasService) private strategiesService: EstrategiasService) {}
 
@@ -81,6 +87,8 @@ export class CircuitsComponent implements OnInit {
   }
 
   showBestStrategies(circuit: any) {
+    this.bestStrategiesVisible = true;
+    this.simuladorActivo = false;
     this.strategiesService.getBestStrategies(circuit).subscribe((strategies: any[]) => {
       this.bestStrategies = strategies;
     });
@@ -97,5 +105,51 @@ export class CircuitsComponent implements OnInit {
       });
     }
     return stops;
+  }
+
+  onSimulador(circuito: any) {
+    this.selectedCircuit = circuito;
+    this.simuladorActivo = true;
+    this.bestStrategiesVisible = false;
+    this.simuladorResultado = null;
+    this.simuladorStints = [];
+    this.simuladorParadas = 1;
+  }
+
+  agregarStint() {
+    this.simuladorStints.push({ compuesto: '', vueltas: null });
+  }
+
+  eliminarStint(i: number) {
+    this.simuladorStints.splice(i, 1);
+    if (this.simuladorStints.length === 0) {
+      this.simuladorResultado = null;
+    }
+  }
+
+  simularEstrategiaLibre() {
+    const totalVueltas = this.simuladorStints.reduce((acc, s) => acc + Number(s.vueltas), 0);
+    const vueltasCircuito = this.selectedCircuit.laps ?? this.selectedCircuit.vueltas;
+
+    if (totalVueltas > vueltasCircuito) {
+      alert(`La suma de vueltas (${totalVueltas}) no puede superar las vueltas del circuito (${vueltasCircuito}).`);
+      return;
+    }
+    if (totalVueltas < vueltasCircuito) {
+      alert(`La suma de vueltas (${totalVueltas}) no puede ser menor que las vueltas del circuito (${vueltasCircuito}).`);
+      return;
+    }
+
+    const estrategia = this.simuladorStints
+      .filter(s => s.compuesto && s.vueltas)
+      .map(s => [s.compuesto.toLowerCase() as "blandos" | "medios" | "duros", Number(s.vueltas)] as ["blandos" | "medios" | "duros", number]);
+    this.strategiesService.simularEstrategiaLibre(this.selectedCircuit, estrategia)
+      .subscribe(res => this.simuladorResultado = res);
+  }
+
+  get vueltasLibres(): number {
+    const vueltasCircuito = this.selectedCircuit?.laps ?? this.selectedCircuit?.vueltas ?? 0;
+    const totalVueltas = this.simuladorStints.reduce((acc, s) => acc + Number(s.vueltas), 0);
+    return vueltasCircuito - totalVueltas;
   }
 }
