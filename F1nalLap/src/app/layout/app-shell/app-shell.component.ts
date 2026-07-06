2f3418a96f9ec2c93f81977b89ea0da08b514593
Alberto <aga.alberto.st@gmail.com>
2026-07-06 19:14:54 +0200

    refactor(layout): extract header/nav/footer into a shared AppShellComponent

---
+++ F1nalLap/src/app/layout/app-shell/app-shell.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { CloseOtherMenusDirective } from '../../Cerrado/cerrado.component';
+import { OverlayStateService } from '../overlay-state.service';
+import { ScrollProgressDirective } from '../scroll-progress.directive';
+
+@Component({
+  selector: 'app-shell',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatMenuModule,
+    MatButtonModule,
+    CloseOtherMenusDirective,
+    ScrollProgressDirective,
+  ],
+  templateUrl: './app-shell.component.html',
+  styleUrl: './app-shell.component.scss',
+})
+export class AppShellComponent {
+  protected readonly overlayState = inject(OverlayStateService);
+}
