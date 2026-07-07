bafe0d8ee49ffe7077ab2b2935f146ca70ce8f1e
Alberto <aga.alberto.st@gmail.com>
2026-07-07 09:27:32 +0200

    feat(ui): unifica el sistema de botones y pule el navbar

--- F1nalLap/src/app/layout/app-shell/app-shell.component.ts
+++ F1nalLap/src/app/layout/app-shell/app-shell.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { CloseOtherMenusDirective } from '../../Cerrado/cerrado.component';
 import { OverlayStateService } from '../overlay-state.service';
 import { ScrollProgressDirective } from '../scroll-progress.directive';
+import { ScrollShadowDirective } from '../scroll-shadow.directive';
 
 @Component({
   selector: 'app-shell',
@@ -15,6 +16,7 @@ import { ScrollProgressDirective } from '../scroll-progress.directive';
     MatButtonModule,
     CloseOtherMenusDirective,
     ScrollProgressDirective,
+    ScrollShadowDirective,
   ],
   templateUrl: './app-shell.component.html',
   styleUrl: './app-shell.component.scss',
